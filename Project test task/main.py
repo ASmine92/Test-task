50e9395354b63b1fb6290703621ec8b4e0d42968
ASmine92 <133440669+ASmine92@users.noreply.github.com>
2025-08-31 18:57:41 +0300

    Script.py
    Main thing

--- Project test task/main.py
+++ Project test task/main.py
@@ -1,20 +1,27 @@
 import requests
 import math
 
-request = requests.get('https://catfact.ninja/facts')
-total_facts = (request.json()['total'])
-facts_per_page = (request.json()['per_page'])
+# Get first page data
+response = requests.get('https://catfact.ninja/facts')
+response = response.json()
+total_facts = response['total']
+facts_per_page = response['per_page']
 
+# Calculate last page
 total_pages = (math.ceil(total_facts/facts_per_page))
 
-request = requests.get('https://catfact.ninja/facts?page='+str(total_pages))
+# Get last page data
+response = requests.get('https://catfact.ninja/facts?page='+str(total_pages))
+response = response.json()
 
-shortest_fact_length = request.json()['data'][0]['length']
+shortest_fact_length = response['data'][0]['length']
 
-for fact_length in request.json()['data']:
+# Figure out shortest fact length
+for fact_length in response['data']:
     if (fact_length['length']) < shortest_fact_length:
         shortest_fact_length = fact_length['length']
 
-for fact in request.json()['data']:
+# Print shortest facts (might be multiple answers)
+for fact in response['data']:
     if fact['length'] == shortest_fact_length:
         print(fact['fact'])
